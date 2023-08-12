bdeaad31c6caecc1665bd2753f8ec906a5ad8e21
Logan James Hart <116290186+loganjameshart@users.noreply.github.com>
2023-08-12 12:49:14 -0500

    Create debiansetup.py

---
+++ debiansetup.py
@@ -0,0 +1,29 @@
+#! /usr/bin/env python3
+'''Quick Debian setup. Run as root!'''
+
+import os
+
+USER = os.getlogin()
+
+PROGRAMS = [
+	'ufw', 'vbetool', 'network-manager', 'htop', 'fail2ban', 'syncthing', 'tmux'
+	]
+
+
+def install(desired_programs: list):
+	for program in desired_programs:
+		os.system(f"sudo apt install {program}")
+
+def ufw_setup():
+	os.system('sudo ufw allow ssh')
+	os.system('sudo ufw allow syncthing')
+	os.system('sudo ufw reload')
+
+def syncthing_setup():
+	os.system('syncthing')
+	os.system(f'sudo systemctl enable syncthing@{USER}.service')
+
+if __name__ == '__main__':
+	install(PROGRAMS)
+	ufw_setup()
+	syncthing_setup()
